18b9e4d881217c51387ac5e03dfcdce177d074d6
Zanzibarr <matteozanella2@gmail.com>
2024-03-01 16:46:57 +0100

    Default values

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main(int argc, const char** argv) {
     }
 
     instance inst;
+    initialize_defaults();
 
     parse_cmd(argc, argv, &inst);
 
@@ -17,10 +18,16 @@ int main(int argc, const char** argv) {
 
 }
 
+void initialize_defaults() {
+
+    time_limit = DEF_TL;
+    seed = -1;
+
+}
+
 void parse_cmd(const int argc, const char** argv, instance* inst) {
 
     char file_name[1000];
-    int seed = -1;
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], FILE) == 0) { strcpy(file_name, argv[++i]); continue; }
--- utils.h
+++ utils.h
@@ -4,11 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
+// PARSING
 #define FILE "-file"
 #define TIME_LIMIT "-tl"
 #define SEED "-seed"
 
-double time_limit;
+// TIME MANAGEMENT
+#define MS_SEC 1e+3
+#define DEF_TL 8.64e+7
+
+// NUMBERS
+#define EPSYLON 1e-9
+
+uint64_t time_limit;
+uint64_t seed;
+
+double random_value() {
+    
+}
 
 #endif
