53e00b5c526848efbf6448b6fee6cc84f075e464
Zanzibarr <matteozanella2@gmail.com>
2024-03-16 15:52:56 +0100

    Multithreading tabu

--- plotting/plot_cost.py
+++ plotting/plot_cost.py
@@ -1,16 +1,23 @@
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
 
-with open(sys.argv[1], "r") as f:
-    sol = f.read()
-    
-lines = sol.splitlines()
+for i in range(0, 12):
+    with open(f"test{i}", "r") as f:
+        sol = f.read()
+        
+    lines = sol.splitlines()
 
-y = []
+    y = []
 
-for line in lines:
-    y_ = line
-    y.append(float(y_))
-    
-plt.plot(range(0, len(y)), y)
+    for line in lines:
+        y.append(float(line))
+
+    best = np.argmin(y)
+        
+    plt.scatter(range(0, len(y)), y, s=1)
+
+    with open(f"test{i}", "w") as f:
+        f.write("")
+
 plt.show()
