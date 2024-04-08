617e19cbcca91a341e01aa5b2950b3a476febe08
Zanzibarr <matteozanella2@gmail.com>
2024-04-08 12:01:58 +0200

    plotting and performance profiler

--- plotting/plot_solution.py
+++ plotting/plot_solution.py
@@ -1,25 +1,19 @@
-import sys
+import sys, os
 import matplotlib.pyplot as plt
 
 with open(sys.argv[1], "r") as f:
     sol = f.read()
     
 lines = sol.splitlines()
-
-alg         = lines[0].partition("Algorithm: ")[2].strip()
-cost        = lines[1].partition("Cost: ")[2].strip()
-time        = lines[2].partition("Time: ")[2].strip()
-total_time  = lines[3].partition("Total execution time: ")[2].strip()
-over_time   = "has" in lines[4]
-
 x, y = [], []
 
-for line in lines[5:]:
-    x_, y_ = line.split(" ")
+for line in lines[lines.index("--------------------")+1:]:
+    _, x_, y_ = line.split()
     x.append(float(x_))
     y.append(float(y_))
     
 plt.plot(x, y)
 plt.plot(x[0], y[0], ".", markersize=14, color="blue")
-plt.figtext(0.5, 0.92, f"{alg} - nodes: {len(x)-1} - cost: {cost} - total time: {total_time}", wrap=True, horizontalalignment='center', fontsize=12)
-plt.show()
+
+#plt.show()
+plt.savefig(f"../plots/{os.path.basename(sys.argv[1]).split('/')[-1].replace("file.txt", "plot.png")}")
---
+++ plotting/pp_autom.py
@@ -0,0 +1,55 @@
+import sys, subprocess, shlex, notify
+
+# FORMAT FOR THE COMMAND FILE:
+#
+# number of istances : common parameters for all istances and algorthms
+# alg1 parameters : name displayed on plot
+# alg2 parameters : name displayed on plot
+# ...
+
+off = (len(sys.argv) == 3 and sys.argv[2] == "off")
+
+bot = notify.bot(profile="default", set_on=not off)
+
+file = sys.argv[1]
+
+with open(file, "r") as f:
+    lines = f.read().splitlines()
+
+algs = lines[1:]
+
+list = [[] for i in range(len(algs))]
+
+n_instances = int(lines[0].partition(":")[0].strip())
+
+print(f"Running {n_instances} from the {file} file.")
+
+for alg in range(len(algs)):
+    for i in range(1, n_instances + 1):
+        command = f"../tsp -seed {i} {lines[0].partition(':')[2].strip()} -alg {algs[alg].partition(':')[0].strip()}"
+        print(f"Running command {command}\n")
+
+        bot.send_message_by_text(command)
+
+        result = subprocess.run(shlex.split(command), capture_output=True, text = True).stdout
+
+        result = result.partition("BEST SOLUTION:")[2].partition("Cost:")[2].partition("\n")[0].strip()
+        list[alg].append(result)
+
+with open(f"{file}_result.csv", "w") as f:
+
+    f.write(f"{len(algs)}")
+    for alg in range(len(algs)): f.write(f", {algs[alg].partition(':')[2].strip()}")
+    f.write("\n")
+
+    for i in range(1, n_instances + 1):
+        f.write(f"{i}")
+        for alg in range(len(algs)):
+            f.write(f", {list[alg][i-1]}")
+        f.write("\n")
+
+max_ratio = '%.2f'%(float(max([max(lst) for lst in list]))/float(min([min(lst) for lst in list]))+.01)
+
+subprocess.run(shlex.split(f'python3 pp.py -D , -T 120 -S 2 -M {max_ratio} {file}_result.csv {file}_result.png -P ""'))
+
+bot.send_photo_by_path(f"{file}_result.png")
