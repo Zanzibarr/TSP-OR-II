6cba351aecb048912941ccdf027367d2c71a132a
Zanzibarr <matteozanella2@gmail.com>
2024-06-11 17:07:07 +0200

    setup for testing

--- pp_autom.py
+++ pp_autom.py
@@ -1,4 +1,4 @@
-import sys, subprocess, shlex, notify, os
+import sys, subprocess, shlex, notify, os, time
 
 # FORMAT FOR THE COMMAND FILE:
 #
@@ -9,6 +9,8 @@ import sys, subprocess, shlex, notify, os
 
 bot = notify.bot(profile="default")
 bot.edit_profile(disable_notification=True)
+
+silent = len(sys.argv) > 2 and sys.argv[2] == "silent"
 
 backup_file = f"{os.path.dirname(__file__)}/backup"
 
@@ -38,10 +40,13 @@ try:
         bot.create_progress_bar(n_instances, f"Algorithm: {algs[alg].partition(': ')[2]}")
 
         for i in range(1, n_instances + 1):
+            
+            start_time = time.time()
+            
             command = f"./tsp -seed {i} {lines[0].partition(':')[2].strip()} -alg {algs[alg].partition(':')[0].strip()}"
             print(f"Running command {command}\n")
 
-            bot.send_message_by_text(command)
+            if not silent: bot.send_message_by_text(command)
 
             result = subprocess.run(shlex.split(command), capture_output=True, text = True).stdout
 
@@ -53,7 +58,12 @@ try:
             list_time[alg].append(result_time)
 
             with open(backup_file, "a") as f:
-                f.write(f"{algs[alg]} - {i} : {result_cost} - {result_time}\n")
+                f.write("{:40} :: {:3} :: {:15.4} :: {:8.4}\n".format(algs[alg].partition(':')[0].strip(), i, result_cost, result_time))
+                # f"{algs[alg]} - {i} : {result_cost} - {result_time}"
+
+            # prevent notify bar update to get a too_many_requests error
+            if (time.time() - start_time < 1):
+                time.sleep(1)
 
             bot.update_progress_bar()
 
@@ -89,7 +99,8 @@ try:
     #subprocess.run(shlex.split(f'python3 plotting/pp.py -X "Cost Ratio" -D , -S 2 {file}_cost_result.csv {file}_cost_result.png -P ""'))
     #subprocess.run(shlex.split(f'python3 plotting/pp.py -X "Time Ratio" -D , -S 2 {file}_time_result.csv {file}_time_result.png -P ""'))
 
-    #bot.on()
+    bot.on()
+    bot.send_message_by_text("Done.")
     #bot.send_photo_by_path(f"{file}_cost_result.png")
     #bot.send_photo_by_path(f"{file}_time_result.png")
 
